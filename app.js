3f1fecc6e4a1589007aaf394833d148c7be1bc9d
Igor Rodrigues <igorjr2003@gmail.com>
2024-09-28 00:44:44 -0300

    PostgreSQL + Docker Compose setup

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const db = require('./db');
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/db', async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM users');
+    res.send(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.send("Error: " + error);
+  }
 })
 
 app.listen(port, () => {
